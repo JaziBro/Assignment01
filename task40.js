b9e393960656ee263c289bdae25586ff0690d155
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-08-31 20:45:22 +0500

    message

--- task40.js
+++ task40.js
@@ -1,15 +1,15 @@
 //Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information.
 // Use the function to make three dictionaries representing different albums. Print each return value to show that Objects are storing the album information correctly. 
 //Add an optional parameter to make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. Make at least one new function call that includes the number of tracks on an album.
-function make_album(artistname: string, albumtitle: string, albumsongs: number) {
-    return { artistname, albumtitle, albumsongs};
+function make_album(artistname, albumtitle, albumsongs) {
+    return { artistname: artistname, albumtitle: albumtitle, albumsongs: albumsongs };
 }
 console.log("Drake", "Take Care", "17");
-function make_album2(artistname: string, albumtitle: string, albumsongs: number) {
-    return { artistname, albumtitle, albumsongs};
+function make_album2(artistname, albumtitle, albumsongs) {
+    return { artistname: artistname, albumtitle: albumtitle, albumsongs: albumsongs };
 }
 console.log("Asim Azhar", "Noor", "5");
-function make_album3(artistname: string, albumtitle: string, albumsongs: number) {
-    return { artistname, albumtitle, albumsongs};
+function make_album3(artistname, albumtitle, albumsongs) {
+    return { artistname: artistname, albumtitle: albumtitle, albumsongs: albumsongs };
 }
 console.log("Talha Anjum", "Open Letter", "15");
---
+++ task41-43.js
@@ -0,0 +1,15 @@
+//Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array
+var Magicians = ["Criss Angel", "David Copperfield", "Harry Houdini"];
+function show_magicinas() {
+    return Magicians;
+}
+console.log(Magicians);
+//Great Magicians: Start with a copy of your program from Exercise 41. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name.
+//Call show_magicians() to see that the list has actually been modified.
+function make_great(name) {
+    if (name === void 0) {
+        name = "Great";
+    }
+    return name + Magicians;
+}
+console.log("Great", Magicians[0], "Great", Magicians[1], "Great", Magicians[2]);
